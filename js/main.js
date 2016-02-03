0ce446337c3a892e0b89490eba352358e35b47cd
Nathan Arthur <narthur.a@gmail.com>
2016-02-03 16:40:54 -0600

    Imported javascript from mockup

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by masteredit3 on 2/3/16.
+ */
+
+jQuery(document).ready(function($) {
+    $('.slider').unslider({
+        nav: false,
+        autoplay: true,
+        delay: 7000,
+        animation: 'fade',
+        animateHeight: true
+    });
+});
