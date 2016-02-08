5485a8a974c47b7cf1ab154dda25b88cf95779a2
Nathan Arthur <narthur.a@gmail.com>
2016-02-08 16:17:33 -0600

    Refactor subpage nav

--- page.php
+++ page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
 	$heroUrl = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
@@ -10,27 +11,11 @@
 		<?php
 		$parents = array_reverse(get_post_ancestors($post->ID));
 		$sectionParent = get_post($parents[1]);
+		$parentId = $sectionParent->ID;
 
 		echo "<h1 class='entry-title'>$sectionParent->post_title</h1>";
 
-		$children = get_pages(array(
-			'child_of' => $sectionParent->ID,
-			'parent' => $sectionParent->ID
-		));
-
-		if (!empty($children)) {
-			$currentId = get_the_ID();
-			$items = '';
-
-			foreach ($children as $child) {
-				$isActive = $child->ID == $currentId;
-				$extraAtts = ($isActive) ? ' class="active"' : '';
-				$format = '<li%s><a href="%s">%s</a></li>';
-				$items .= sprintf( $format, $extraAtts, get_page_uri($child), $child->post_title );
-			}
-
-			echo "<ul class='subpages nav nav-pills nav-stacked'>$items</ul>";
-		}
+		subpageNav( $parentId );
 		?>
 	</div>
 	<section class="entry-content">
