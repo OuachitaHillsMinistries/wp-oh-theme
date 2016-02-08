ba57db67865b8b346c1b62c270b747148a92a2e9
Nathan Arthur <narthur.a@gmail.com>
2016-02-08 10:41:30 -0600

    Added sub-nav to page template

--- page.php
+++ page.php
@@ -7,11 +7,32 @@
 	if ($heroUrl) echo "<div style=\"background-image: url('$heroUrl');\" class=\"header jumbotron\"></div>";
 	?>
 	<div class="meta">
-		<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+		<?php
+		$parents = array_reverse(get_post_ancestors($post->ID));
+		$categoricalParent = get_post($parents[1]);
+		$title = get_the_title();
+		$firstParentChildren = get_children(array(
+			'post_parent' => $categoricalParent->ID,
+			'post_type' => 'page'
+		));
+		echo "<h1 class='entry-title'>$categoricalParent->post_title</h1>";
+		if (!empty($firstParentChildren)) {
+			$childLinks = wp_list_pages(array(
+				'child_of' => $categoricalParent->ID,
+				'title_li' => '',
+				'echo'     => false,
+				'depth'    => 2,
+				'walker'   => new bootstrap_pills_walker()
+			));
+			echo "<ul class='subpages nav nav-pills nav-stacked'>$childLinks</ul>";
+		}
+		?>
 	</div>
 	<section class="entry-content">
-		<?php /*if ( has_post_thumbnail() ) { the_post_thumbnail(); }*/ ?>
-		<div class="text"><?php the_content(); ?></div>
+		<div class="text">
+			<?php the_content(); ?>
+			<?php edit_post_link(); ?>
+		</div>
 		<div class="entry-links"><?php wp_link_pages(); ?></div>
 		<?php ohImageGallery(); ?>
 	</section>
