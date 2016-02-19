972f21c41f8f49f8d0327d61486d991bb409455f
Nathan Arthur <narthur.a@gmail.com>
2016-02-19 09:56:17 -0600

    Add cropper controls

--- admin.js
+++ admin.js
@@ -28,22 +28,28 @@ jQuery(document).ready(function ($) {
         var cropper =
             '<div class="featuredCropper">' +
             '<div id="featuredWrapper"><img src="' + image.url + '" /><div class="drag"></div></div>' +
+            '<div class="cropperControls">' +
+            '<p>Please drag the highlighted region up and down to select the primary portion of this image.</p>' +
+            '<a href="#" class="button button-large">Cancel</a> <a href="#" class="button button-primary button-large">Save</a>' +
+            '</div>' +
             '</div>';
 
         $('body').append(cropper);
 
-        resizeFeaturedWrapper();
+        resizeCropperElements();
         initDragWindow();
+
     }
 
-    function resizeFeaturedWrapper() {
+    function resizeCropperElements() {
         var featuredWrapper = $('#featuredWrapper');
+        $('.cropperControls').css('width', image.width);
         featuredWrapper.css('width', image.width);
         featuredWrapper.css('height', image.height);
     }
 
     function initDragWindow() {
-        var drag_height = (2 * image.width) / 19;
+        var drag_height = (2 * image.width) / 15;
         var drag_window = $('.drag');
         drag_window.css({
             'height': drag_height,
@@ -55,10 +61,7 @@ jQuery(document).ready(function ($) {
             drag: function () {
                 var y1 = drag_window.position().top;
                 var y2 = y1 + drag_height;
-
-                console.log(y1 + ', ' + y2 + ', ' + calculatePercent(y1, y2));
-                console.log(image.height);
-                console.log(image)
+                drag_window.css('background-position','0 ' + calculatePercent(y1,y2) + '%');
             }
         })
     }
